041f12a0eb9b7c1d40d6a69c23c8f9f04d7a6536
lassandra85 <lassandra85@gmail.com>
2025-04-07 22:41:43 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,17 @@
-function getElementWidth(content, padding, border) {
-    const total =
-        Number.parseFloat(content) +
-        2 * Number.parseFloat(padding) +
-        2 * Number.parseFloat(border);
+function filterArray(numbers, value) {
+    let newArray = [];
 
-    return total;
+    for (const item of numbers) {
+        if (item > value) {
+            newArray.push(item);
+        }
+    }
+
+    return newArray;
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
